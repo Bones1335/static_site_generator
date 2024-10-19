8c724cf2e7916ea0930bd4510fd2b2a7882e8ea5
Alexander Hansen <ahansen1335@gmail.com>
2024-10-19 07:57:02 +0200

    create markdown image and link extraction functions with tests

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,23 @@
+import unittest
+
+from extract_markdown import (
+    extract_markdown_links,
+    extract_markdown_images
+)
+
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        links = extract_markdown_links(text)
+        self.assertEqual(links, [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        images = extract_markdown_images(text)
+        self.assertEqual(images, [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
+
+
+
+if __name__ == "__main__":
+    unittest.main()
