aacb4663712a46c13519b43e0ce39da168865338
Alexander Hansen <ahansen1335@gmail.com>
2024-10-19 08:10:45 +0200

    refactor extract_markdown and split delimiter functions into one file called inline markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,4 @@
+import re
 from textnode import (
     TextNode,
     TextType
@@ -9,6 +10,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT.value:
             new_nodes.append(old_node)
+            continue
         split_nodes = []
         sections = old_node.text.split(delimiter)
         if len(sections) % 2 == 0:
@@ -23,3 +25,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,12 +1,26 @@
 import unittest
 
-from extract_markdown import (
+from textnode import (
+    TextNode,
+    TextType
+)
+from inline_markdown import (
     extract_markdown_links,
-    extract_markdown_images
+    extract_markdown_images,
+    split_nodes_delimiter
 )
 
 
-class TestSplitDelimiter(unittest.TestCase):
+class TestInlineMarkdown(unittest.TestCase):
+    def test_code_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+            ])
+
     def test_extract_markdown_links(self):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         links = extract_markdown_links(text)
@@ -16,7 +30,6 @@ class TestSplitDelimiter(unittest.TestCase):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
         images = extract_markdown_images(text)
         self.assertEqual(images, [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")])
-
 
 
 if __name__ == "__main__":
