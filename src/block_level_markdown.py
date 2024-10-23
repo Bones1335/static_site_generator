57d6ec3c95d4e5f09d1c41d946f7da3c4f3bf012
Alexander Hansen <ahansen1335@gmail.com>
2024-10-23 15:18:28 +0200

    create block to block type function with tests

--- src/block_level_markdown.py
+++ src/block_level_markdown.py
@@ -11,3 +11,32 @@ def markdown_to_blocks(markdown):
             new_blocks.append(block.strip())
     new_blocks.append("\n".join(list_block))
     return new_blocks
+
+
+def block_to_block_type(markdown_block):
+    markdown_code = markdown_block[:markdown_block.index(' ')]
+    if markdown_code[0] == "#":
+        return "heading"
+    elif markdown_code[0:3] == "```" and markdown_block[-3:] == "```":
+        return "code"
+    elif markdown_code == ">":
+        temp = markdown_block.split("\n")
+        for i in temp:
+            if i[0] != markdown_code:
+                raise ValueError("Invalid markdown, not a block quote")
+        return "quote"
+    elif markdown_code == "*" or markdown_code == "-":
+        temp = markdown_block.split("\n")
+        for i in temp:
+            if i[0] != markdown_code:
+                raise ValueError("Invalid markdown, not an unordered list block")
+        return "unordered list"
+    elif markdown_code == "1.":
+        temp = markdown_block.split("\n")
+        for num in temp:
+            if int(num[0]) != int(markdown_code[0]) + (int(num[0]) - 1):
+                raise ValueError("Invalid markdown, not an ordered list block")
+        return "ordered list"
+    else:
+        return "paragraph"
+
--- src/test_block_level_markdown.py
+++ src/test_block_level_markdown.py
@@ -1,12 +1,13 @@
 import unittest
 
 from block_level_markdown import (
-    markdown_to_blocks
+    markdown_to_blocks,
+    block_to_block_type
 )
 
 
 class TestBlockLevelMarkdown(unittest.TestCase):
-    def test_code_delimiter(self):
+    def test_markdown_to_blocks(self):
         markdown = """# This is a heading
 
 This is a paragraph of text. It has some **bold** and *italic* words inside of it.
@@ -21,6 +22,36 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
 * This is a list item
 * This is another list item"""])
 
-            
+    def test_block_to_block_type_heading(self):
+        block = "###### This is a heading"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, "heading")
+
+    def test_block_to_block_type_code(self):
+        block = "```\n This is a code block \n```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, "code")
+
+    def test_block_to_block_type_quote(self):
+        block = "> This is a quote block\n> Line 2 of quote block\n> Line 3 of quote block"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, "quote")
+
+    def test_block_to_block_type_unordered_list(self):
+        block = "* This is an unordered list block\n* Line 2 of unordered list block\n* Line 3 of unordered list block"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, "unordered list")
+
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. This is an ordered list block\n2. Line 2 of ordered list block\n3. Line 3 of ordered list block"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, "ordered list")
+
+    def test_block_to_block_type_paragraph(self):
+        block = "This is a paragraph block"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, "paragraph")
+
+
 if __name__ == "__main__":
     unittest.main()
