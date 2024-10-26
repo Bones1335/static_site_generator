c49130746511a3397833418ced623d3d01547500
Alexander Hansen <ahansen1335@gmail.com>
2024-10-26 10:43:21 +0200

    create recursive generate pages function

--- src/generate_page.py
+++ src/generate_page.py
@@ -4,25 +4,39 @@ from block_level_markdown import markdown_to_html_node
 from extract_title import extract_title
 
 
+def generate_pages(from_path, template_path, to_path):
+    print(os.listdir(from_path))
+    for content in os.listdir(from_path):
+        content_path = os.path.join(from_path, content)
+
+        if os.path.isfile(content_path):
+            generate_page(content_path, template_path, os.path.join(to_path, content.replace(".md", ".html")))
+        if os.path.isdir(content_path):
+            if not os.path.exists(os.path.join(to_path, content)):
+                os.makedirs(content, exist_ok=True)
+            generate_pages(content_path, template_path, os.path.join(to_path, content))
+
+
 def generate_page(from_path, template_path, to_path):
     print(f"Generating page from {from_path} to {to_path} using {template_path}")
+    markdown = open(from_path, 'r')
+    md_contents = markdown.read()
+    markdown.close()
 
     template = open(template_path, 'r')
     template_file = template.read()
     template.close()
 
-    for filename in os.listdir(from_path):
-        markdown = open(os.path.join(from_path, filename), 'r')
-        md_contents = markdown.read()
-        markdown.close()
+    title = extract_title(md_contents)
 
-        title = extract_title(md_contents)
-        html_nodes = markdown_to_html_node(md_contents)
-        html_string = html_nodes.to_html()
-        
-        template_title = template_file.replace("{{ Title }}", title)
-        template_content = template_title.replace("{{ Content }}", html_string)
+    html_nodes = markdown_to_html_node(md_contents)
+    html_string = html_nodes.to_html()
 
-        with open(f"{to_path}/index.html", "w", encoding="utf-8") as f:
-            f.write(template_content)
-        
+    template_title = template_file.replace("{{ Title }}", title)
+    template_content = template_title.replace("{{ Content }}", html_string)
+
+    to_dir_path = os.path.dirname(to_path)
+    if to_dir_path != "":
+        os.makedirs(to_dir_path, exist_ok=True)
+    to_file = open(to_path, "w")
+    to_file.write(template_content)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from copy_static import copy_files
-from generate_page import generate_page
+from generate_page import generate_pages
 
 
 def main():
@@ -10,6 +10,7 @@ def main():
     static = f"{root}/static"
     public = f"{root}/public"
     content = f"{root}/content"
+    template = f"{root}/template.html"
 
     print("Deleting public directory...")
     if os.path.exists(public):
@@ -17,7 +18,9 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files(static, public)
-    generate_page(content, f"{root}/template.html", public)
+
+    print("Generating page...")
+    generate_pages(content, template, public)
 
 
 if __name__ == "__main__":
